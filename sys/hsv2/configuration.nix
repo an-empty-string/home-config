176fcc41c9933f6dc5f18701b96dafce4dc417ae
Tris Emmy Wilson <tris@tris.fyi>
2022-09-14 19:54:51 -0500

    hsv2 update

--- sys/hsv2/configuration.nix
+++ sys/hsv2/configuration.nix
@@ -18,5 +18,28 @@
   networking.interfaces.enp2s0.useDHCP = true;
   boot.kernel.sysctl."net.ipv4.ip_forward" = "1";
 
+  virtualisation.docker.enable = true;
+
+  networking.wireguard.interfaces.wg0 = {
+    ips = [
+      "fdd2:972a:b48c::1/64"
+      "100.127.255.1/24"
+    ];
+
+    peers = [
+      {
+        publicKey = "EPtzMkbJF6vGOgQdZUv4sIk7yNp6PRUzdyl+2zoOY2s=";
+        allowedIPs = [
+          "fdd2:972a:b48c::2/128"
+          "100.127.255.2/32"
+        ];
+      }
+    ];
+
+    listenPort = 51821;
+    privateKeyFile = "/etc/wireguard/wg0.key";
+    generatePrivateKeyFile = true;
+  };
+
   system.stateVersion = "22.05";
 }
