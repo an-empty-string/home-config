228d56355e4f41aa539e745c558ef7d493fd59f2
Tris Emmy Wilson <tris@tris.fyi>
2023-09-18 18:36:33 -0400

    fix efi errors

--- sys/beacon/configuration.nix
+++ sys/beacon/configuration.nix
@@ -6,7 +6,9 @@
     ../modules/common.nix
   ];
 
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.devices = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "beacon";
   time.timeZone = "American/New_York";
